98f609bb590e58a392c05493765d098dbe1c14c3
mmr.K <lmimsra07@gmail.com>
2018-06-13 23:33:50 +0900

    test write

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,35 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <p v-if="msg.length > 0">{{msg}}</p>
+    <p v-else>no text</p>
+    <label for="in_text">入力</label>
+    <input id="in_text" placeholder="input here" type="text" v-model="msg"/>
+    <button @click="clear()">clear</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    data(){
+        return{
+            msg:"Hello World"
+        }
+    },
+    methods: {
+        clear () {
+            this.msg = '';
+        }
+    }
+
+
+
+  // name: 'app',
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
 
