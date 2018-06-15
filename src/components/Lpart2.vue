4da241d6bd5e1cb54eef0be7f302ac240b162695
mmr.K <lmimsra07@gmail.com>
2018-06-16 08:02:55 +0900

    add lesson2

---
+++ src/components/Lpart2.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="part2">
+        <p>Mustache構文→{{msg}}</p>
+        directive↓<p v-text="msg"></p>
+        <p v-pre>式無効化{{msg2}}</p>
+        <p>{{innerhtml}}</p>
+        <p v-html="innerhtml"></p>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Lpart2",
+        data(){
+            return {
+                msg:"Sample Text",
+                msg2:"v-preで式無効化",
+                innerhtml:"<h3>こんにちは！</h3>"
+            }
+        }
+    }
+
+
+</script>
+
+<style scoped>
+
+</style>
