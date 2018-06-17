053f545d5a801c118cf0a6f61f4a9acbdf79e3c3
mmr.K <lmimsra07@gmail.com>
2018-06-17 23:04:25 +0900

    finish section2

--- src/components/Lpart2.vue
+++ src/components/Lpart2.vue
@@ -5,6 +5,18 @@
         <p v-pre>式無効化{{msg2}}</p>
         <p>{{innerhtml}}</p>
         <p v-html="innerhtml"></p>
+        <p>属性値にJS式を埋め込む</p>
+        <!--<a href="{{url}}">うまく動かないリンク</a> エラーになる-->
+        <a :href="url">うまく動くリンク</a>
+        <p>プロパティ値の変更->{{email.split('@')[0].toLowerCase()}}</p>
+        <p>Computedとmethod</p>
+        <p>{{localEmail}}</p>
+        <p>{{localEmailF()}}</p>
+        <input type="button" @click="onclick()" value="更新"/>
+        <p>算出プロパティ：{{randomc}}</p>
+        <p>メソッド：{{randomm()}}</p>
+        <p>現在日時：{{current}}</p>
+
 
     </div>
 </template>
@@ -12,11 +24,34 @@
 <script>
     export default {
         name: "Lpart2",
-        data(){
+        data() {
             return {
-                msg:"Sample Text",
-                msg2:"v-preで式無効化",
-                innerhtml:"<h3>こんにちは！</h3>"
+                msg: "Sample Text",
+                msg2: "v-preで式無効化",
+                innerhtml: "<h3>こんにちは！</h3>",
+                url: "https://www.lmimsra.com",
+                email: "Yamada@example.com",
+                current: new Date().toLocaleString()
+            }
+        },
+        computed:{
+            localEmail: function () {
+                return this.email.split('@')[0].toLowerCase();
+            },
+            randomc: function () {
+                return Math.random();
+            },
+
+        },
+        methods: {
+            localEmailF: function () {
+                return this.email.split('@')[0].toUpperCase();
+            },
+            onclick: function () {
+                this.current = new Date().toLocaleString()
+            },
+            randomm: function () {
+                return Math.random();
             }
         }
     }
