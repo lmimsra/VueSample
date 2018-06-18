864b92320c4207a94c721ec195ab84657637bfbf
mmr.K <lmimsra07@gmail.com>
2018-06-18 22:26:04 +0900

    add basic event method

--- src/components/Lpart3.vue
+++ src/components/Lpart3.vue
@@ -1,11 +1,36 @@
 <template>
-    
+    <div>
+        <p>イベントの基本</p>
+        <button v-on:click="onclick">bindクリック</button>
+        <button @click="onclick">@クリック</button>
+        <p>now->{{message}}</p>
+        <button @click="getEvent('こんにちは',$event)">イベント情報取得</button>
+    </div>
 </template>
 
 <script>
+    /* eslint-disable no-console */
+
+
     export default {
-        name: "Lpart3"
+        name: "Lpart3",
+        data() {
+            return {
+                message:"",
+            }
+        },
+        methods:{
+            onclick:function (e) {
+                this.message = new Date().toLocaleString();
+                console.log(e);
+            },
+            getEvent:function (message,e) {
+              console.log(message);
+              console.log(e);
+            }
+        }
     }
+
 </script>
 
 <style scoped>
