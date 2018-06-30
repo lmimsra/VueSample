c0b5cfb3a0846d39f7251ac6fbec624c236a3f5f
mmr.K <lmimsra07@gmail.com>
2018-06-30 18:44:57 +0900

    more bindセクション 途中まで

--- src/components/Lpart6.vue
+++ src/components/Lpart6.vue
@@ -1,12 +1,29 @@
 <template>
     <div>
-
+        <strong>multi bind</strong><br/>
+        <label for="message">メッセージ</label>
+        <input id="message" type="text" v-bind="attrs"/><br/>
+        <strong>v-once</strong><br/>
+        <label for="name">氏名：</label>
+        <input type="text" id="name" v-model="name"/>
+        <p v-once>{{name}}</p>
+        <p>{{name}}</p>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Lpart6"
+        name: "Lpart6",
+        data() {
+            return {
+                attrs: {
+                    size: 20,
+                    maxLength: 15,
+                    required: true,
+                },
+                name:'ゲスト'
+            }
+        }
     }
 </script>
 
